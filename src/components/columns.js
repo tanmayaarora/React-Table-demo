0df279c9b5cab19bfd16aab8010fda57a09988b4
tanmayaarora <tanmaya.arora@chd.trantorinc.com>
2022-02-17 16:05:32 +0530

    First commit

---
+++ src/components/columns.js
@@ -0,0 +1,24 @@
+export const COLUMNS = [
+    {
+        Header: 'Id',
+        accessor: 'id'
+    },
+    {
+        Header: 'First Name',
+        accessor:'first_name'
+    },
+    {
+        Header: 'Last Name',
+        accessor: 'last_name'
+    },
+    {
+        Header: 'Date of Birth',
+        accessor: 'date_of_birth'
+    },
+    {
+        Header: 'Country'
+    },
+    {
+        Header: 'Phone'
+    }
+]
